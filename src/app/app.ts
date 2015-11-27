c70f763ad6786ab2ab37f7698d04b1382bcef110
robin <engel.robin@gmx.de>
2015-11-27 11:12:38 +0100

    dummy kvp table

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,10 @@
 import {bootstrap, Component} from 'angular2/angular2';
+import {KVPTable} from './modules/kvpTable';
 @Component({
     selector: 'my-app',
     templateUrl: 'app/templates/app/app.html',
-    styleUrls: ['app/templates/app/app.css']
+    styleUrls: ['app/templates/app/app.css'],
+    directives: [KVPTable]
 })
 class AppComponent { }
 bootstrap(AppComponent);
---
+++ src/app/modules/kvpTable.ts
@@ -0,0 +1,13 @@
+import {boostrap, Component} from 'angular2/angular2'
+
+@Component({
+    selector: 'kvp-table',
+    templateUrl: 'app/templates/kvpTable/kvpTable.html',
+    styleUrls: ['app/templates/kvpTable/kvpTable.css']
+})
+export class KVPTable {
+
+
+
+}
+
