273db8cd0fcd6720fac459a66c88639fd86100d5
Max <max-geissel@web.de>
2016-01-13 13:59:59 +0100

    Login created.

--- app/templates/dashboard/dashboard.component.ts
+++ app/templates/dashboard/dashboard.component.ts
@@ -7,10 +7,25 @@ import {NgFor} from 'angular2/common';
 })
 export class DashboardComponent {
   private tickets: any = [
-    {"name": "Ticket 1"},
-    {"name": "Ticket 2"},
-    {"name": "Ticket 3"},
-    {"name": "Ticket 4"},
-    {"name": "Ticket 5"}
+    {
+      "name": "Ticket 1",
+      "status": "Offen"
+    },
+    {
+      "name": "Ticket 2",
+      "status": "In Bearbeitung"
+    },
+    {
+      "name": "Ticket 3",
+      "status": "Offen"
+    },
+    {
+      "name": "Ticket 4",
+      "status": "Geschlossen"
+    },
+    {
+      "name": "Ticket 5",
+      "status": "In Bearbeitung"
+    }
   ];
 }
--- app/templates/navigation/navigation.component.ts
+++ app/templates/navigation/navigation.component.ts
@@ -1,6 +1,12 @@
 import {Component} from 'angular2/core';
+import {NgIf} from 'angular2/common';
 @Component({
     selector: 'navigation',
-    templateUrl: 'app/templates/navigation/navigation.html'
+    templateUrl: 'app/templates/navigation/navigation.html',
+    directives: [NgIf]
 })
-export class NavigationComponent { }
+export class NavigationComponent {
+  isLoggedIn(): boolean {
+    return true;
+  }
+}
