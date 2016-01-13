fc228e4ec6493b105a2dee52c1611a5872331959
Max <max-geissel@web.de>
2016-01-13 13:41:37 +0100

    TicketView in Dashboard added.

--- app/templates/dashboard/dashboard.component.ts
+++ app/templates/dashboard/dashboard.component.ts
@@ -1,6 +1,16 @@
 import {Component} from 'angular2/core';
+import {NgFor} from 'angular2/common';
 
 @Component({
-    templateUrl: 'app/templates/dashboard/dashboard.html'
+    templateUrl: 'app/templates/dashboard/dashboard.html',
+    directives: [NgFor]
 })
-export class DashboardComponent { }
+export class DashboardComponent {
+  private tickets: any = [
+    {"name": "Ticket 1"},
+    {"name": "Ticket 2"},
+    {"name": "Ticket 3"},
+    {"name": "Ticket 4"},
+    {"name": "Ticket 5"}
+  ];
+}
