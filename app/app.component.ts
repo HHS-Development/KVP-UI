e169182d84c8f9d1ade7da368ce51b0df276ae41
Max <max-geissel@web.de>
2016-01-13 13:33:15 +0100

    Dashboard added. Routerlink added and set dashboard to default.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,14 @@
 import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {NavigationComponent} from './templates/navigation/navigation.component';
+import {DashboardComponent} from './templates/dashboard/dashboard.component';
+
 @Component({
     selector: 'app-component',
     templateUrl: 'app/templates/app/app.html',
-    directives: [NavigationComponent]
+    directives: [ROUTER_DIRECTIVES, NavigationComponent]
 })
+@RouteConfig([
+  {path:'/home', name: 'Dashboard', component: DashboardComponent, useAsDefault: true}
+])
 export class AppComponent { }
---
+++ app/templates/dashboard/dashboard.component.ts
@@ -0,0 +1,6 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    templateUrl: 'app/templates/dashboard/dashboard.html'
+})
+export class DashboardComponent { }
