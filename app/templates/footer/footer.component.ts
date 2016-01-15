73caa4aeaf9cb5b3a384bdbbcf34a5dd188589e6
Max <max-geissel@web.de>
2016-01-15 11:01:41 +0100

    Footer added. Home page added to routerlink.

---
+++ app/templates/footer/footer.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'footer-component',
+    templateUrl: 'app/templates/footer/footer.html',
+    styleUrls: ['app/css/styles.css']
+})
+export class FooterComponent { }
