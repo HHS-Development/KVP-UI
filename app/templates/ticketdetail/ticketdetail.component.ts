409f6881b08a83e0594a1b640162c929c5f3cc02
Max <max-geissel@web.de>
2016-01-22 11:41:36 +0100

    Ticket Detail now shows Ticket Data

--- app/templates/ticketdetail/ticketdetail.component.ts
+++ app/templates/ticketdetail/ticketdetail.component.ts
@@ -5,9 +5,31 @@ import {RouteParams, Router} from "angular2/router";
     templateUrl: "app/templates/ticketdetail/ticketdetail.html"
 })
 export class TicketDetailComponent {
-  private ticket: any;
+  private _ticket: any;
   constructor(private _router: Router, private _routeParams: RouteParams) { }
   ngOnInit() {
-    this.ticket = this._routeParams.get("ticket");
+    this._ticket = this._routeParams.get("ticket");
+  }
+
+  getOwner(): string{
+    // todo: Ownername by ticket.ownerId
+    return "Charlie Sheen";
+  }
+
+  getOwnerCourse(): string{
+    // todo: get Course by ticket.ownerId from LDAP
+    return "E3FI2";
+  }
+
+  getCreatedDate(): string{
+    return this._ticket.createdDate;
+  }
+
+  getSubject(): string{
+    return this._ticket.subject;
+  }
+
+  getProposal(): string{
+    return this._ticket.proposal;
   }
 }
